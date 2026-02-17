8c7982ce9170763f222b42e3381d6fdccc0d2192
sonal-aot <sonal.sai@aot-technologies.com>
2026-02-17 12:17:46 +0530

    M8F-42 : [ feat ] Add connector proxy service with dedicated Dockerfile and update default application and proxy ports.

--- connector-proxy-demo/app.py
+++ connector-proxy-demo/app.py
@@ -15,4 +15,4 @@ if app.config.get("ENV", "development") != "production":
 app.register_blueprint(proxy_blueprint)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=7004)
+    app.run(host="0.0.0.0", port=8004)
