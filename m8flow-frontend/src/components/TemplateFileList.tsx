67bac3d4b30ce44abf9472d573d92b451d5ec077
abilpraju-aot <abil.raju@aot-technologies.com>
2026-06-22 22:56:10 -0700

    fixed ci

--- m8flow-frontend/src/components/TemplateFileList.tsx
+++ m8flow-frontend/src/components/TemplateFileList.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import { useTranslation } from "react-i18next";
 import { Visibility, GetApp } from "@mui/icons-material";
 import {
   Box,
